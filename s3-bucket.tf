9d3a11b27f1a1c5b36a56d798eec1e0546b06886
Simon Archer <simon.james.archer@gmail.com>
2021-10-14 11:52:01 +0000

    Added s3 bucket

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,12 +1,5 @@
-// module "s3_bucket" {
-//   source = "terraform-aws-modules/s3-bucket/aws"
-
-//   bucket = "my-s3-bucket"
-//   acl    = "private"
-//   bucket_prefix = "hashicorp-aws-workshop23"
-
-//   versioning = {
-//     enabled = true
-//   }
-
-// }
+module "s3-bucket" {
+  source  = "app.terraform.io/simonarcher/s3-bucket/aws"
+  version = "2.2.0"
+  bucket_prefix = "hashicorp_aws_workshop32"
+}
